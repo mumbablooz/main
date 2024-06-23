6651098c05d53b9aedb735bc92289c6124c35592
Blooz <89782115+mumbablooz@users.noreply.github.com>
2024-06-23 11:38:28 +0200

    Update ShowPicture.jsx

--- ShowImage/ShowPicture.jsx
+++ ShowImage/ShowPicture.jsx
@@ -1,15 +1,24 @@
 import React from 'react'
 import Image from 'next/image'
 
-export default function ShowPicture({array,index,height}){
+export default function ShowPicture({ array, index,width, height, pictureLink }) {
+if(!array) return (<Image
+  src={'/' + pictureLink}
+  width={width}
+  height={height}
+  alt={pictureLink}
+  style={{
+    width: 'auto',
+    height: '20rem',
+  }} />)
 
-    return (<Image 
-      src={'/'+array[index].name} 
-      width={array[index].width} 
-      height={array[index].height} 
-      alt={array[index].describtion}
-      style={{
-        width: 'auto',
-        height: height ? height : '3em',
-      }}/> )
-        }
+  return (<Image
+    src={'/' + array[index].name}
+    width={array[index].width}
+    height={array[index].height}
+    alt={array[index].describtion}
+    style={{
+      width: 'auto',
+      height: height ? height : '3em',
+    }} />)
+}
